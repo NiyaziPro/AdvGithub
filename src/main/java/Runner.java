7024d05a9c1c0c406d0af2e35187717f2a4e48e5
NiyaziPro <nyzjunior@gmail.com>
2024-10-11 09:34:13 +0200

    version3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Version 1");
         System.out.println("Version 2");
+        System.out.println("Version 3");
     }
 }
